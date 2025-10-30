7a83384330076173cc0522d81ed738ec66a2d577
fpjosesoriano-creator <fp_josesoriano@ciudadjardin.org>
2025-10-30 13:11:02 +0100

    Comienzo practica2 y ejercicio1

---
+++ Practica2/Ejercicio1.py
@@ -0,0 +1,11 @@
+# Ejercicio 3: Número par o impar
+# Crear una función que nos diga si un número es par o impar.
+def ParoImpar(num1):  
+    if num1 % 2 == 0:
+        print(f"El numero {num1} es par")
+    else:
+        print(f"El numero {num1} no es par")
+
+ParoImpar(224)
+
+
