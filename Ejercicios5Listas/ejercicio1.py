2419ba282f4c8a1cb3a662729da308e5b1bd6213
fpjosesoriano-creator <fp_josesoriano@ciudadjardin.org>
2025-11-12 10:31:03 +0100

    Ejercicios apartado 5 start

---
+++ Ejercicios5Listas/ejercicio1.py
@@ -0,0 +1,7 @@
+
+#1. La función min(lista) devuelve el valor más pequeño contenido en una lista.
+# #Crea una función que devuelva el número más pequeño en una lista de números. 
+# Hazlo con bucle for sin utilizar la función min.
+
+#Author:Jsoriano
+
